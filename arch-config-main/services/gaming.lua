20a70f62537483d3ba320a49bd648048d04eeabf
DonBaal <sebastian.birner@brz.eu>
2026-02-05 14:35:16 +0100

    delete

--- arch-config-main/services/gaming.lua
+++
@@ -1,8 +0,0 @@
-return {
-    description = "Gaming and streaming services",
-    services = {
-        enabled = { "sunshine.service" },
-        disabled = {},
-    },
-    conflicts = { "server" },
-}
